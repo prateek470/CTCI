3321fb8f8a448b1f597bd5efc888b2213cfdb5c1
Prateek Gupta <prateek470@gmail.com>
2016-11-07 23:46:50 -0600

    added trees

--- src/TreesAndGraphs/BalancedTree.java
+++ src/TreesAndGraphs/BalancedTree.java
@@ -27,7 +27,9 @@ public class BalancedTree {
 		
 		return Math.abs(maxheight(root)-minheight(root))<=1;
 	}
-	
+	/*
+	 * Find Max Depth of the tree;
+	 */
 	private static int maxheight(TreeNode root) {
 		if(root==null) return 0;
 		return 1+Math.max(maxheight(root.left), maxheight(root.right));
@@ -37,11 +39,37 @@ public class BalancedTree {
 		if(root==null) return 0;
 		return 1+Math.max(minheight(root.left), minheight(root.right));
 	}
+	
+	/*
+	 * Create tree with minimal height 
+	 */
+	
+	public static TreeNode createMinimalTree(int[] arr){
+		return createMinimalTree(arr,0,arr.length-1);
+	}
+	
+	private static TreeNode createMinimalTree(int[] arr, int start, int end) {
+		if(end<start)
+			return null;
+		
+		int mid = start + (end-start)/2;
+		TreeNode root = new TreeNode(mid);
+		
+		root.left = createMinimalTree(arr, start, mid-1);
+		root.right = createMinimalTree(arr, mid+1, end);
+		
+		return root;
+	}
+	/*
+	 * Given a sorted (increasing order) array, write an algorithm to create a binary tree with minimal height.
+	 */
 	public static void main(String[] args) {
-		TreeNode root = new TreeNode(1);
-		root.left = new TreeNode(2);
-		root.right = new TreeNode(4);
-		root.left.left = new TreeNode(3);
-		System.out.println(TreeBalanced(root));
+//		TreeNode root = new TreeNode(1);
+//		root.left = new TreeNode(2);
+//		root.right = new TreeNode(4);
+//		root.left.left = new TreeNode(3);
+//		System.out.println(TreeBalanced(root));
+//		
+		
 	}
 }
